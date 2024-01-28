bfdc66d229e20e5629902599c822c02f9e365a36
jamal <jamalkacade@gmail.com>
2024-01-28 22:49:10 +0800

    Got an error saying: AttributeError: module 'streamlit' has no attribute 'column_config'

--- Intro/Features/Data_elements.py
+++ Intro/Features/Data_elements.py
@@ -497,6 +497,30 @@ st.divider()
 
 
 
+df = pd.DataFrame(
+    {
+        "name": ["Roadmap", "Extras", "Issues"],
+        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        "stars": [np.random.randint(0, 1000) for _ in range(3)],
+        "views_history": [[np.random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+    }
+)
+st.dataframe(
+    df,
+    column_config={
+        "name": "App name",
+        "stars": st.column_config.NumberColumn(
+            "Github Stars",
+            help="Number of stars on GitHub",
+            format="%d ⭐",
+        ),
+        "url": st.column_config.LinkColumn("App URL"),
+        "views_history": st.column_config.LineChartColumn(
+            "Views (past 30 days)", y_min=0, y_max=5000
+        ),
+    },
+    hide_index=True,
+)
 
 
 
