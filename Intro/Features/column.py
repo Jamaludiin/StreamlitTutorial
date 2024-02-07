8378c4902551933721088af09c1529b09ed66ed0
jamal <jamalkacade@gmail.com>
2024-02-07 10:36:00 +0800

    working with st.column

--- Intro/Features/column.py
+++ Intro/Features/column.py
@@ -1,8 +1,17 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 
 
+# def
+def my_code(code):
+    st.code(f"""{code}""")
+#------------------------------------------------------------
+# syntax 
 # st.columns(spec, *, gap="small")
+st.title("1: Example of st.columns element")
+
+st.subheader("Display and group elements under Columns")
 
 col1, col2, col3 = st.columns(3, gap="large")
 
@@ -21,3 +30,48 @@ with col3:
    st.title("This is a TITLE of COLUMN THREE")
    st.header("Another HEADER")
    st.button("Button")
+
+
+#_____________________________________________________________
+st.title("12: Code of the Example above")
+
+code_example = """import streamlit as st
+import pandas as pd
+
+st.subheader("Display and group elements under Columns")
+
+col1, col2, col3 = st.columns(3, gap="large")
+
+with col1:
+   st.title("This is a TITLE of COLUMN ONE")
+   st.header("This is a HEADER")
+   st.subheader("This is SUBHEADER")
+   st.write("This is a WRITE")
+
+with col2:
+    st.title("This is a TITLE of COLUMN TWO")
+    st.checkbox("This CheckBox")
+    st.multiselect("This CheckBox",['A','B','C','D'])
+
+with col3:
+   st.title("This is a TITLE of COLUMN THREE")
+   st.header("Another HEADER")
+   st.button("Button")
+"""
+my_code(code_example)
+st.divider()
+
+
+#------------------------------------------------------------
+# syntax 
+# st.columns(spec, *, gap="small")
+st.subheader("Display charts and other data elements under Columns")
+
+col1, col2 = st.columns([3, 1])
+data = np.random.randn(10, 1)
+
+col1.subheader("Column one with a chart")
+col1.line_chart(data)
+
+col2.subheader("Column two with the data")
+col2.write(data)
