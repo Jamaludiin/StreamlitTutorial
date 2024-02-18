2d8064b01a710eeb2010466788388838a9b3e4a7
jamal <jamalkacade@gmail.com>
2024-02-18 15:44:01 +0800

    working with st.altair_chart updated with new weather data and diffrent features of graphs, updated

--- Intro/Features/Chart elements/altair_chart.py
+++ Intro/Features/Chart elements/altair_chart.py
@@ -325,8 +325,11 @@ st.divider()
 # syntax 
 # st.altair_chart(altair_chart, use_container_width=False, theme="streamlit")
 
+# this code is from streamlit website https://docs.streamlit.io/library/api-reference/charts/st.altair_chart
 # Data Retrieval from the data.seattle_weather
 # This line presumably retrieves weather data for Seattle using a function called seattle_weather() from a dataset named data.
+st.subheader("5: Example of st.altair_chart using the Altair library with Weather Data")
+
 source = data.seattle_weather()
 
 # Color Scale: This section defines a color scale mapping weather conditions (sun, fog, drizzle, rain, snow) to specific colors.
@@ -387,3 +390,78 @@ with tab1:
     st.altair_chart(chart, theme="streamlit", use_container_width=True)
 with tab2:
     st.altair_chart(chart, theme=None, use_container_width=True)
+
+
+#_____________________________________________________________
+st.title("4: Code of the Example above")
+
+code_example = """import streamlit as st
+import pandas as pd
+import altair as alt
+from vega_datasets import data
+
+st.subheader("5: Example of st.altair_chart using the Altair library with Weather Data")
+
+source = data.seattle_weather()
+
+# Color Scale: This section defines a color scale mapping weather conditions (sun, fog, drizzle, rain, snow) to specific colors.
+scale = alt.Scale(
+    domain=["sun", "fog", "drizzle", "rain", "snow"],
+    range=["#e7ba52", "#a7a7a7", "#aec7e8", "#1f77b4", "#9467bd"],
+)
+color = alt.Color("weather:N", scale=scale)
+
+# We create two selections:
+# - a brush that is active on the top panel
+# - a multi-click that is active on the bottom panel
+brush = alt.selection_interval(encodings=["x"])
+click = alt.selection_multi(encodings=["color"])
+
+# Top panel is scatter plot of temperature vs time
+points = (
+    alt.Chart()
+    .mark_point()
+    .encode(
+        alt.X("monthdate(date):T", title="Date"),
+        alt.Y(
+            "temp_max:Q",
+            title="Maximum Daily Temperature (C)",
+            scale=alt.Scale(domain=[-5, 40]),
+        ),
+        color=alt.condition(brush, color, alt.value("lightgray")),
+        size=alt.Size("precipitation:Q", scale=alt.Scale(range=[5, 200])),
+    )
+    .properties(width=550, height=300)
+    .add_selection(brush)
+    .transform_filter(click)
+)
+
+# Bottom Panel Bar Chart: Bottom panel is a bar chart of weather type
+bars = (
+    alt.Chart()
+    .mark_bar()
+    .encode(
+        x="count()",
+        y="weather:N",
+        color=alt.condition(click, color, alt.value("lightgray")),
+    )
+    .transform_filter(brush)
+    .properties(
+        width=550,
+    )
+    .add_selection(click)
+)
+
+# Concatenated Chart: This concatenates the two charts vertically (vconcat) and sets the title as "Seattle Weather: 2012-2015".
+chart = alt.vconcat(points, bars, data=source, title="Seattle Weather: 2012-2015")
+
+# Streamlit Tabs for Chart Themes:
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
+
+with tab1:
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab2:
+    st.altair_chart(chart, theme=None, use_container_width=True)
+"""
+my_code(code_example)
+st.divider()
