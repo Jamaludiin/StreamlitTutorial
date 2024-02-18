3bc964e4775d29f3be22f284397314f8fa2f4295
jamal <jamalkacade@gmail.com>
2024-02-18 16:26:53 +0800

    working with st.vega_lite_chart updated again

--- Intro/Features/Chart elements/vega_lite_chart.py
+++ Intro/Features/Chart elements/vega_lite_chart.py
@@ -129,3 +129,41 @@ st.vega_lite_chart(
 """
 my_code(code_example)
 st.divider()
+
+#------------------------------------------------------------
+# syntax 
+# st.vega_lite_chart(data=None, spec=None, use_container_width=False, theme="streamlit", **kwargs)
+st.subheader("3: Example of st.vega_lite_chart")
+
+# this code is from streamlit website https://docs.streamlit.io/library/api-reference/charts/st.vega_lite_chart
+
+source = data.cars()
+
+chart = {
+    "mark": "point",
+    "encoding": {
+        "x": {
+            "field": "Horsepower",
+            "type": "quantitative",
+        },
+        "y": {
+            "field": "Miles_per_Gallon",
+            "type": "quantitative",
+        },
+        "color": {"field": "Origin", "type": "nominal"},
+        "shape": {"field": "Origin", "type": "nominal"},
+    },
+}
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Vega-Lite native theme"])
+
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.vega_lite_chart(
+        source, chart, theme="streamlit", use_container_width=True
+    )
+with tab2:
+    st.vega_lite_chart(
+        source, chart, theme=None, use_container_width=True
+    )
