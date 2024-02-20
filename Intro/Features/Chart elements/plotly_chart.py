7a489af24258fef62c5b3c4ea0622de573b2f3ef
jamal <jamalkacade@gmail.com>
2024-02-20 14:44:02 +0800

    working with st.map

---
+++ Intro/Features/Chart elements/plotly_chart.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# def
+def my_code(code):
+    st.code(f"""{code}""")
+
+#------------------------------------------------------------
+# syntax 
+# st.plotly_chart(figure_or_data, use_container_width=False, sharing="streamlit", theme="streamlit", **kwargs)
+
+st.subheader("1: Example of st.plotly_chart DO IT IN THE FUTURE")
