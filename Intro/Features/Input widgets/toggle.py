41a221679176a355bee4d4f14fedbdc3a397a322
jamal <jamalkacade@gmail.com>
2024-02-21 22:37:07 +0800

    Working with st.toggle updated

--- Intro/Features/Input widgets/toggle.py
+++ Intro/Features/Input widgets/toggle.py
@@ -30,3 +30,42 @@ if pypl:
 """
 my_code(code_example)
 st.divider()
+
+
+
+#------------------------------------------------------------
+# syntax 
+# st.toggle(label, value=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+st.subheader("2: Example of st.toggle")
+
+pypl1 = st.toggle("Select Payment Plan",value=True)
+pypl2 = st.toggle("Select non Payment Plan",value=False)
+
+
+if pypl1:
+    st.write("You have selected Payment Plan Option")
+
+elif pypl2:
+    st.write("You have selected Non Payment Plan Option")
+
+
+
+#_____________________________________________________________
+st.title("1: Code of the Example above")
+
+code_example = """import streamlit as st
+
+st.subheader("2: Example of st.toggle")
+
+pypl1 = st.toggle("Select Payment Plan",value=True)
+pypl2 = st.toggle("Select non Payment Plan",value=False)
+
+
+if pypl1:
+    st.write("You have selected Payment Plan Option")
+
+elif pypl2:
+    st.write("You have selected Non Payment Plan Option")
+"""
+my_code(code_example)
+st.divider()
