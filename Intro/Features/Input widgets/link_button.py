c0a3578db4cff97faa8f96c253ca65b6d8cd412b
jamal <jamalkacade@gmail.com>
2024-02-21 22:00:38 +0800

    Working with link_button updated the file

--- Intro/Features/Input widgets/link_button.py
+++ Intro/Features/Input widgets/link_button.py
@@ -24,3 +24,29 @@ st.write("State of the BUTTON:", visited)
     
 
 st.link_button("Go to Github", "https://github.com/Jamaludiin",disabled=False, type="primary", use_container_width=True)
+
+#_____________________________________________________________
+st.title("1: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("1: Example of st.link_button")
+
+# Create a link button
+btn1 = st.link_button("Go to transformers", "https://huggingface.co/docs/transformers/en/installation", use_container_width=True)
+
+# Display different messages based on whether the button is clicked
+if btn1:
+    st.write("Wow you visited the Hugging Face Transformers installation Guide")
+    visited = True
+else:
+    visited = False
+
+# Display the state of the button
+st.write("State of the BUTTON:", visited)
+    
+
+st.link_button("Go to Github", "https://github.com/Jamaludiin",disabled=False, type="primary", use_container_width=True)
+
+"""
+my_code(code_example)
+st.divider()
