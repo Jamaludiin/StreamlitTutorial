a295d50d965516b6e43296bb280a3ce2464e5042
jamal <jamalkacade@gmail.com>
2024-02-22 16:11:15 +0800

    working with st.selectbox updated

--- Intro/Features/Input widgets/selectbox.py
+++ Intro/Features/Input widgets/selectbox.py
@@ -54,7 +54,7 @@ selected_option = st.selectbox(
 st.write('You selected to pay through:', selected_option)
 
 #_____________________________________________________________
-st.title("1: Code of the Example above")
+st.title("2: Code of the Example above")
 
 code_example = """import streamlit as st
 st.subheader("2: Example of st.selectbox")
@@ -74,6 +74,50 @@ selected_option = st.selectbox(
 )
 
 st.write('You selected to pay through:', selected_option)
+"""
+my_code(code_example)
+st.divider()
+
+
+#------------------------------------------------------------
+# syntax 
+# st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder="Choose an option", disabled=False, label_visibility="visible")
+
+st.subheader("3: Example of st.selectbox")
+
+option_zero = "Not selected any plan yet"
+
+option = st.selectbox(
+   'Select the payment option you want?',
+    ["Master Card", "Online Banking", "Pay by Cash"],
+   index=None,
+   placeholder="Select the payment plans....",
+)
+
+if option is None:
+ st.write('You selected to pay through:', option_zero)
+else:
+ st.write('You selected to pay through:', option)
+   
+#_____________________________________________________________
+st.title("3: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("3: Example of st.selectbox")
+
+option_zero = "Not selected any plan yet"
+
+option = st.selectbox(
+   'Select the payment option you want?',
+    ["Master Card", "Online Banking", "Pay by Cash"],
+   index=None,
+   placeholder="Select the payment plans....",
+)
+
+if option is None:
+ st.write('You selected to pay through:', option_zero)
+else:
+ st.write('You selected to pay through:', option)
 """
 my_code(code_example)
 st.divider()
