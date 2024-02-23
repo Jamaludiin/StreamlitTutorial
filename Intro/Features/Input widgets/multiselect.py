c71731451f44185aa75b307cb794c88ca617d52a
jamal <jamalkacade@gmail.com>
2024-02-23 17:55:42 +0800

    working with st.multiselect updated

--- Intro/Features/Input widgets/multiselect.py
+++ Intro/Features/Input widgets/multiselect.py
@@ -47,6 +47,8 @@ st.divider()
 
 #------------------------------------------------------------
 # syntax 
+# st.multiselect(label, options, default=None, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, max_selections=None, placeholder="Choose an option", disabled=False, label_visibility="visible")
+
 st.subheader("2: Example of st.multiselect")
 
 selected_option= st.multiselect(label="Select the payment method",
@@ -85,3 +87,25 @@ st.divider()
 
 #------------------------------------------------------------
 # syntax 
+# st.multiselect(label, options, default=None, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, max_selections=None, placeholder="Choose an option", disabled=False, label_visibility="visible")
+
+st.subheader("3: Example of st.multiselect")
+
+selected_option1= st.multiselect(label="Select the payment method",
+               options=["Master Card", "Online Banking", "Pay by Cash"],
+               placeholder="Choose an option",
+
+               disabled=False,
+               label_visibility="visible",
+               key="multiselect"
+               
+)
+
+if "Master Card" in selected_option1:
+    st.write('You selected to pay through:', selected_option1)
+elif "Online Banking" in selected_option1:
+    st.write('You selected to pay through:', selected_option1)
+elif "Pay by Cash" in selected_option1:
+    st.write('You selected to pay through:', selected_option1)
+else:
+    st.write('You selected either all or nothing:', selected_option1)
