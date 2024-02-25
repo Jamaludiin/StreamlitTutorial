78ed4970534c8d905bd92ea374873443e064dc3d
jamal <jamalkacade@gmail.com>
2024-02-25 16:14:22 +0800

    working with st.number_input updated

--- Intro/Features/Input widgets/number_input.py
+++ Intro/Features/Input widgets/number_input.py
@@ -12,4 +12,45 @@ st.subheader("1: Example of st.number_input")
 
 st.number_input(label="What is your Lucky Number")
 
-st.number_input(label="What is your age", min_value=1, max_value=100,value=10,step=2)
+#_____________________________________________________________
+st.title("1: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("1: Example of st.number_input")
+
+st.number_input(label="What is your Lucky Number")
+"""
+my_code(code_example)
+st.divider()
+
+#------------------------------------------------------------
+# syntax 
+# st.number_input(label, min_value=None, max_value=None, value="min", step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
+st.subheader("2: Example of st.number_input")
+st.number_input(label="What is your age", 
+                min_value=1, 
+                max_value=100,
+                value=10,
+                step=2,
+                help="Only a number",
+                placeholder="Numbers",
+                disabled=False,
+                label_visibility='visible')
+
+#_____________________________________________________________
+st.title("1: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("2: Example of st.number_input")
+st.number_input(label="What is your age", 
+                min_value=1, 
+                max_value=100,
+                value=10,
+                step=2,
+                help="Only a number",
+                placeholder="Numbers",
+                disabled=False,
+                label_visibility='visible')
+"""
+my_code(code_example)
+st.divider()
