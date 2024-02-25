3bb836e3d57a7f1ce1454f2ec4fa69741c9083a4
jamal <jamalkacade@gmail.com>
2024-02-25 16:20:40 +0800

    working with st.number_input updated again

--- Intro/Features/Input widgets/number_input.py
+++ Intro/Features/Input widgets/number_input.py
@@ -54,3 +54,52 @@ st.number_input(label="What is your age",
 """
 my_code(code_example)
 st.divider()
+
+
+#------------------------------------------------------------
+# syntax 
+# st.number_input(label, min_value=None, max_value=None, value="min", step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
+st.subheader("3: Example of st.number_input")
+
+lucky_num = st.number_input(label="Tell me your Lucky Number")
+st.write("Your lucky number is:",lucky_num)
+
+#_____________________________________________________________
+st.title("3: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("3: Example of st.number_input")
+
+lucky_num = st.number_input(label="Tell me your Lucky Number")
+
+st.write("Your lucky number is:",lucky_num)
+"""
+my_code(code_example)
+st.divider()
+
+
+#------------------------------------------------------------
+# syntax 
+# st.number_input(label, min_value=None, max_value=None, value="min", step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
+st.subheader("4: Example of st.number_input")
+
+lucky_num = st.number_input("Insert your lucky number", 
+                            value=None, 
+                            placeholder="Type the number")
+
+st.write('Your lucky number is', lucky_num)
+
+#_____________________________________________________________
+st.title("4: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("4: Example of st.number_input")
+
+lucky_num = st.number_input("Insert your lucky number", 
+                            value=None, 
+                            placeholder="Type the number")
+
+st.write('Your lucky number is', lucky_num)
+"""
+my_code(code_example)
+st.divider()
