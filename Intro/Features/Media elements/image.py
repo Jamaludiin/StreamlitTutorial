78e3e56c8b0a5b7b8f85522cc0820e34f75debaf
jamal <jamalkacade@gmail.com>
2024-03-01 21:21:32 +0800

    working with st.image update also

--- Intro/Features/Media elements/image.py
+++ Intro/Features/Media elements/image.py
@@ -27,6 +27,28 @@ st.divider()
 #------------------------------------------------------------
 # syntax 
 # st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-st.subheader("1: Example of st.image")
+st.subheader("2: Example of st.image")
 
-st.image('Python Tuples.png', caption='Python Tuples cover page', width=1000,use_column_width=False,clamp=True,output_format="PNG")
+st.image('Python Tuples.png', 
+         caption='Python Tuples cover page', 
+         width=1000,
+         use_column_width=False,
+         clamp=True,
+         output_format="PNG")
+
+
+#_____________________________________________________________
+st.title("2: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("2: Example of st.image")
+
+st.image('Python Tuples.png', 
+         caption='Python Tuples cover page', 
+         width=1000,
+         use_column_width=False,
+         clamp=True,
+         output_format="PNG")
+"""
+my_code(code_example)
+st.divider()
