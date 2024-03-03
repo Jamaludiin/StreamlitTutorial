e62970104425d21cc7cd5df538de3e6c19854049
jamal <jamalkacade@gmail.com>
2024-03-03 20:16:22 +0800

    working with st.sidebar and other elements updated once again

--- Intro/Features/Layouts and containers/sidebar.py
+++ Intro/Features/Layouts and containers/sidebar.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import time 
 
 # def
 def my_code(code):
@@ -101,7 +101,7 @@ with st.sidebar:
 
 
 #_____________________________________________________________
-st.title("1: Code of the Example above")
+st.title("2: Code of the Example above")
 
 code_example = """import streamlit as st
 st.subheader("2: Example of st.sidebar 'with' notation")
@@ -127,6 +127,52 @@ with st.sidebar:
         ("Card", "Cash", "Loan", "Online Banking"),
         key="select_slider"
     )
+"""
+my_code(code_example)
+st.divider()
+
+
+#------------------------------------------------------------
+# syntax 
+# st.sidebar.[element_name]
+# "with" notation
+#with st.sidebar:
+   # st.[element_name]
+
+st.subheader("3: Example of st.sidebar with aditional practice")
+
+with st.sidebar:
+    st.divider()
+    st.subheader("st.sidebar with new practice")
+    with st.echo():
+        st.write("This is a code displayed on the side bar")
+
+    with st.spinner("In Progress please wait..."):
+        time.sleep(5)
+    st.success("Success message!")
+    st.warning("This is a warning message!")
+    st.error("Error happen! message")
+
+
+#_____________________________________________________________
+st.title("3: Code of the Example above")
+
+code_example = """import streamlit as st
+import time 
+
+st.subheader("3: Example of st.sidebar with aditional practice")
+
+with st.sidebar:
+    st.divider()
+    st.subheader("st.sidebar with new practice")
+    with st.echo():
+        st.write("This is a code displayed on the side bar")
+
+    with st.spinner("In Progress please wait..."):
+        time.sleep(5)
+    st.success("Success message!")
+    st.warning("This is a warning message!")
+    st.error("Error happen! message")
 """
 my_code(code_example)
 st.divider()
