af3c2920710fe5d4afeb286ebe3338816a70398d
jamal <jamalkacade@gmail.com>
2024-03-03 20:09:35 +0800

    working with st.sidebar and other elements updated again

--- Intro/Features/Layouts and containers/sidebar.py
+++ Intro/Features/Layouts and containers/sidebar.py
@@ -75,7 +75,7 @@ st.divider()
 #with st.sidebar:
    # st.[element_name]
 
-st.subheader("1: Example of st.sidebar 'with' notation")
+st.subheader("2: Example of st.sidebar 'with' notation")
 # Using "with" notation
 with st.sidebar:
     st.divider()
@@ -98,3 +98,35 @@ with st.sidebar:
         ("Card", "Cash", "Loan", "Online Banking"),
         key="select_slider"
     )
+
+
+#_____________________________________________________________
+st.title("1: Code of the Example above")
+
+code_example = """import streamlit as st
+st.subheader("2: Example of st.sidebar 'with' notation")
+# Using "with" notation
+with st.sidebar:
+    st.divider()
+    st.subheader("st.sidebar 'with' notation")
+
+    add_selectbox = st.sidebar.multiselect(
+    "How would you like to pay your bills?",
+    ("Card", "Cash", "Loan", "Online Banking"),
+    key='multiselect1'
+    )
+
+    add_selectbox = st.sidebar.radio(
+        "How would you like to pay your bills?",
+        ("Card", "Cash", "Loan", "Online Banking"),
+        key="radio1"
+    )
+
+    add_selectbox = st.sidebar.select_slider(
+        "How would you like to pay your bills?",
+        ("Card", "Cash", "Loan", "Online Banking"),
+        key="select_slider"
+    )
+"""
+my_code(code_example)
+st.divider()
