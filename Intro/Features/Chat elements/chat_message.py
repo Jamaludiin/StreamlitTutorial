6dc79ae7d164cb6be4ecf6878e68b9c49dcd7093
jamal <jamalkacade@gmail.com>
2024-03-11 23:08:34 +0800

    chat_message practice with code section displayed

--- Intro/Features/Chat elements/chat_message.py
+++ Intro/Features/Chat elements/chat_message.py
@@ -16,12 +16,48 @@ with st.chat_message("user"):
     st.write("Hello 👋")
     st.line_chart(np.random.randn(30, 3))
 
+#_____________________________________________________________
+st.title("1: Code of the Example above")
 
+code_example = """import streamlit as st
+import numpy as np
+
+st.subheader("1: Example of st.chat_message")
+
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+"""
+my_code(code_example)
+st.divider()
+
+#_____________________________________________________________
+st.title("2: Code of the Example above")
 # without using the with statement
 mesg = st.chat_message("user")
 mesg.write("Hello 👋")
-mesg.line_chart(np.random.randn(30, 3))
+mesg.area_chart(np.random.randn(30, 3))
 mesg.subheader("This is part is part of the message")
 mesg.title("This is a title")
 
 st.subheader("This is part is out of the message")
+
+#_____________________________________________________________
+st.title("1: Code of the Example above")
+
+code_example = """import streamlit as st
+import numpy as np
+
+st.title("2: Code of the Example above")
+# without using the with statement
+mesg = st.chat_message("user")
+mesg.write("Hello 👋")
+mesg.area_chart(np.random.randn(30, 3))
+mesg.subheader("This is part is part of the message")
+mesg.title("This is a title")
+
+st.subheader("This is part is out of the message")
+"""
+my_code(code_example)
+st.divider()
